725ec9029c68a502e2f9f53d6798fc7b575c44a1
anxpp <anxppp@163.com>
2016-02-26 18:01:03 +0800

    Initial commit

---
+++ app/src/main/java/com/anxpp/underlinetextview/MainActivity.java
@@ -0,0 +1,10 @@
+package com.anxpp.underlinetextview;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
